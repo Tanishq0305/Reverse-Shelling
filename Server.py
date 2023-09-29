418a511a309aaef9968a442052ea689c6690bf90
Tanishq Todkari <tanishktodkari03@gmail.com>
2023-09-29 13:07:13 +0530

    Modified Code

--- Server.py
+++ Server.py
@@ -9,9 +9,7 @@ SEPARATOR = "<SEPARATOR>"
 def recv_file():
     received = target.recv(BUFFER_SIZE).decode()
     filename, filesize = received.split(SEPARATOR)
-    # remove absolute path if there is
     filename = os.path.basename(filename)
-    # convert to integer
     # filesize = int(filesize)
     
     # progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
@@ -20,16 +18,11 @@ def recv_file():
             # read 1024 bytes from the socket (receive)
             bytes_read = target.recv(BUFFER_SIZE)
             if not bytes_read:    
-                # nothing is received
-                # file transmitting is done
                 print("file received")
                 break
-            # write to the file the bytes we just received
             f.write(bytes_read)
-            # update the progress bar
             # progress.update(len(bytes_read))
 
-    # close the client socket
     target.close()
     
 
